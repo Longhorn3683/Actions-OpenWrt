a860789e5eba921fefae3bb0f558b14e7bec66b7
Longhorn3683 <52536396+Longhorn3683@users.noreply.github.com>
2020-04-22 09:55:46 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,8 +8,11 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-# 获取Lean的无线驱动
-svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/mt package/mt
+# 获取Lean的部分软件包
+svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-arpbind package/lean/luci-app-arpbind
+svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-autoreboot package/lean/luci-app-autoreboot
+svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-guest-wifi package/lean/luci-app-guest-wifi
+svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/mt package/lean/mt
 
 # 更新feeds
 ./scripts/feeds update -a
