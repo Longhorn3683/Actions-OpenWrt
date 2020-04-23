0a12691f25b511fe0d0ade39fa2449b3c6e30a15
Longhorn3683 <Longhorn3683@outlook.com>
2020-04-23 09:18:13 +0800

    移除PSG1218

--- k2p.sh
+++ k2p.sh
@@ -5,8 +5,6 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # 获取Lean的部分软件包
 svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-arpbind package/lean/luci-app-arpbind
