308be24d1f949d9bd78d51b7135fbea3763d57e1
Longhorn3683 <Longhorn3683@outlook.com>
2020-06-26 20:47:39 +0800

    回归OpenWrt

--- k2p.sh
+++ k2p.sh
@@ -5,12 +5,3 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
-
-# 获取Lean的部分软件包
-svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-arpbind package/lean/luci-app-arpbind
-svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-autoreboot package/lean/luci-app-autoreboot
-svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean/mt package/lean/mt
-
-# 更新feeds
-./scripts/feeds update -a
-./scripts/feeds install -a
